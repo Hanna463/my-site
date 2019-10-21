e6d0bdfd22e0dad88d3601604cf7a3a779467348
Hanna463 <aniamarko21@gmail.com>
2019-10-21 09:45:43 +0000

    new

---
+++ js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+function smoothScroll(Element) {
+
+    Element = document.getElementById(Element);
+    const selectedPosX = 0;
+    const selectedPosY = 0;
+    while (Element != null) {
+        selectedPosX += Element.offsetLeft;
+        selectedPosY += Element.offsetTop;
+        Element = Element.offsetParent;
+    }
+    window.scrollTo(selectedPosX, selectedPosY);
+    }
+
+   document.getElementsByClassName('buttons').mousemove = function() {
+     allert = "Push me"
+       }
